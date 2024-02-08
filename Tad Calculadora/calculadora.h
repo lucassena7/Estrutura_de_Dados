913aaf0fa4693488b2c87f0daeb59a9b0316e307
lucassena7 <90392891+lucassena7@users.noreply.github.com>
2024-02-08 10:46:12 -0300

    Create calculadora.h

---
+++ Tad Calculadora/calculadora.h
@@ -0,0 +1,28 @@
+typedef struct rac Racional;
+
+int mdc(int a, int b);
+
+// funções exportadas
+// cria um número Racional
+Racional *rac_cria(int x, int y);
+
+// Destrói um número Racional criado
+void rac_libera(Racional *r);
+
+// soma dois racionais
+Racional *rac_add(Racional *x, Racional *y);
+
+// subtrai dois racionais
+Racional *rac_sub(Racional *x, Racional *y);
+
+// Multiplica dois racionais
+Racional *rac_mul(Racional *x, Racional *y);
+
+// divide dois racionais
+Racional *rac_divide(Racional *x, Racional *y);
+
+// retorna uma string que representa o Racional
+void rac_exibe(Racional *x);
+
+// verifica se dois numeros racionais são iguais
+int rac_iguais(Racional *x, Racional *y);
