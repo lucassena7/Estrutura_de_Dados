bdd5c92d5739a78a851e2b1653fdadf06370f70e
lucassena7 <90392891+lucassena7@users.noreply.github.com>
2024-02-08 11:34:13 -0300

    Create main.c

---
+++ LSEClassifSemRepComCabeca/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "LSEclassifSemRepComNoCabeca.h"
+int main(void) {
+  tLista * pL;
+  pL=lista_cria();
+  
+  lista_inclui(pL,10);
+  lista_exibe(pL);
+  lista_inclui(pL,20);
+  lista_exibe(pL);
+  lista_inclui(pL,15);
+  lista_exibe(pL);
+  lista_inclui(pL,5);
+  lista_exibe(pL);
+  lista_inclui(pL,35);
+  lista_exibe(pL);
+
+  
+  
+  return 0;
+}
