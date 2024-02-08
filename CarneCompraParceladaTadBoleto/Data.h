b13f4de8288b9f175309929a0094a6ca558c79c4
lucassena7 <90392891+lucassena7@users.noreply.github.com>
2024-02-08 11:15:38 -0300

    Create Data.h

---
+++ CarneCompraParceladaTadBoleto/Data.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Representação do TAD: TData
+//-----------
+// Interface
+//-----------
+
+typedef struct data tData;
+
+/* Funções exportadas */
+/* Função cria - Aloca e retorna uma data dia/mês/ano */
+tData *dta_cria(int dia, int mes, int ano);
+
+/* Função libera � Libera  mem�ria de uma Data criada */
+void dta_libera(tData *p);
+
+/* Fun�o acessa - Retorna os valores de uma Data */
+void dta_acessa(tData *p, int *dia, int *mes, int *ano);
+
+/* Fun�o atribui - Atribui novos valores � Data */
+void dta_atribui(tData *p, int dia, int mes, int ano);
+
+/* Fun�o Intervalo - Retorna a data ap�s X dias */
+tData *dta_intervalo(tData *p1, int X);
+
+/* Fun�o Exibe � Escreve na tela a data  no formato dd/mm/aaaa */
+void dta_exibe(tData *p1);
+
+/* Fun�o getData - Retorna uma string com os valores da data no formato
+ * dd/mm/aaaa*/
+char *dta_getData(tData *p);
+
+tData *somar_dias(tData *data, int num_dias);
